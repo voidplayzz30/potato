2cacaa6fd553ef2898b20df6d860f8509ecdb425
voidplayzz30 <vanshraja16@gmail.com>
2026-05-15 22:24:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,16 @@
-const CACHE_NAME = "potato-v5";
+const CACHE_NAME = "potato-v6";
 const urlsToCache = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
-    "./bg.jpg",
-    "./icon.png"
+    "./", "./index.html", "./style.css", "./script.js",
+    "./manifest.json", "./bg.jpg", "./icon.png"
 ];
-
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache).catch(err => console.log("Cache failed:", err));
-        })
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache).catch(err => console.log("Cache failed:", err))));
     self.skipWaiting();
 });
-
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
-
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
-    );
+    event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
     self.clients.claim();
 });
